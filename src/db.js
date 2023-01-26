8a512a3d4f8daef57981877c757d65f10c0dc9e0
Jhonatan Yaranga <jhoonatan_0912@hotmail.com>
2023-01-26 16:40:54 -0500

    refactor api using env file

--- src/db.js
+++ src/db.js
@@ -1,8 +1,15 @@
 import { createPool } from "mysql2/promise";
 
+import {
+  DB_HOST,
+  DB_USER,
+  DB_PASSWORD,
+  DB_DATABASE
+} from "./config.js";
+
 export const pool = createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '1234',
-  database: 'premierleague'
-})
+  host: DB_HOST,
+  user: DB_USER,
+  password: DB_PASSWORD,
+  database: DB_DATABASE
+});
